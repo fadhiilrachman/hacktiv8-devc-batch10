96cb5eff9ef7ea18f770e77fd486b7455e843c61
Fadhiil Rachman <fadhiilrachman@gmail.com>
2019-03-20 14:38:17 +0700

    Update some javascript

--- chapter3/main.js
+++ chapter3/main.js
@@ -1,34 +1,36 @@
 // main.js
-var count=0, goSearch = function() {
+var count, html, printData = function(search=false) {
+    count=0,html='';
+    planet_data.forEach(function(planet) {
+        count++;
+        name=planet.name;
+        diameter=planet.diameter;
+        climate=planet.climate;
+        population=planet.population;
+        if(planet.name.toLocaleLowerCase()==search&&search!=name) {
+            html+='<tr style="background-color: yellow;">';
+        } else {
+            html+='<tr>';
+        }
+        html+='<td>'+count+'.</td>';
+        html+='<td>'+name+'</td>';
+        html+='<td>'+diameter+'</td>';
+        html+='<td>'+climate+'</td>';
+        if(population!=='unknown') {
+            html+='<td>'+population+'</td>';
+        } else {
+            html+='<td>0</td>';
+        }
+        html+='</tr>';
+    });
+    document.getElementById('doPrint').innerHTML=html;
+}, goSearch = function() {
     txt = document.getElementById('search').value;
     if(txt!=='') {
-        planet_data.forEach(function(planet) {
-            if(planet.name.toLocaleLowerCase()==txt) {
-                alert('Nama planet tersedia');
-            }
-        });
+        printData(txt);
     } else {
         alert('Kata pencarian tidak boleh kosong!');
     }
-}, html='';
+};
 
-planet_data.forEach(function(planet) {
-    count++;
-    name=planet.name;
-    diameter=planet.diameter;
-    climate=planet.climate;
-    population=planet.population;
-    html+='<tr>';
-    html+='<td>'+count+'.</td>';
-    html+='<td>'+name+'</td>';
-    html+='<td>'+diameter+'</td>';
-    html+='<td>'+climate+'</td>';
-    if(population!=='unknown') {
-        html+='<td>'+population+'</td>';
-    } else {
-        html+='<td>0</td>';
-    }
-    html+='</tr>';
-});
-
-document.getElementById('doPrint').innerHTML=html;
+printData();
